de57d199393b79f534dfc055a2f3f1790be71de9
olizet <tymon.zietek@gmail.com>
2019-02-09 12:13:37 +0100

    init-replicator

---
+++ dataReplicator/src/main/java/com/sportsDataAnlyze/dataReplicator/DataReplicatorApplication.java
@@ -0,0 +1,14 @@
+package com.sportsDataAnlyze.dataReplicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataReplicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataReplicatorApplication.class, args);
+	}
+
+}
+

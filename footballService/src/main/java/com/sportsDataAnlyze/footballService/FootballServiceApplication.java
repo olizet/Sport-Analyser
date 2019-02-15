3dcd76808c465ae286a120eaf04da7415773f262
olizet <tymon.zietek@gmail.com>
2019-02-15 23:37:13 +0100

    football service init

---
+++ footballService/src/main/java/com/sportsDataAnlyze/footballService/FootballServiceApplication.java
@@ -0,0 +1,14 @@
+package com.sportsDataAnlyze.footballService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballServiceApplication.class, args);
+	}
+
+}
+
---
+++ footballService/src/test/java/com/sportsDataAnlyze/footballService/FootballServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sportsDataAnlyze.footballService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FootballServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

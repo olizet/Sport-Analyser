781ef42a9df24768594f047bb7161fea881f29b4
olizet <tymon.zietek@gmail.com>
2019-04-09 06:58:06 +0200

    Replicator - Abstract Class and RefereeTask refactor completed + additional smaller things

--- dataReplicator/src/main/java/com/sportsDataAnlyze/dataReplicator/dao/RefereeDao.java
+++ dataReplicator/src/main/java/com/sportsDataAnlyze/dataReplicator/dao/RefereeDao.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface RefereeDao extends PagingAndSortingRepository<Referee,Long> {
 

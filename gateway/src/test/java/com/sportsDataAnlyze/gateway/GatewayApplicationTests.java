6e2cffbb212cb933041571f4503419bf97108dc5
olizet <tymon.zietek@gmail.com>
2019-11-17 15:57:18 +0100

    adds Gateway application

--- gateway/src/test/java/com/sportsDataAnlyze/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/sportsDataAnlyze/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sportsDataAnlyze.footballService;
+package com.sportsDataAnlyze.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FootballServiceApplicationTests {
+public class GatewayApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
 }
-

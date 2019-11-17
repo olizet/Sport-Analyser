add5633b4ad9b5d8d308381d235f1b416ad7afd3
olizet <tymon.zietek@gmail.com>
2019-11-17 15:53:10 +0100

    remove league field from Referee entity

--- dataReplicator/src/main/java/com/sportsDataAnlyze/dataReplicator/service/task/app/RefereeAppTask.java
+++ dataReplicator/src/main/java/com/sportsDataAnlyze/dataReplicator/service/task/app/RefereeAppTask.java
@@ -10,15 +10,14 @@ import java.sql.Statement;
 @Service
 public class RefereeAppTask extends ReplicationTask {
     private static final String QUERY=
-            "insert into football.referee(ref_name,avg_cards,league,matches) \n" +
+            "insert into football.referee(ref_name,avg_cards,matches) \n" +
             " select \n" +
             "f.ref_name, \n" +
             "round(avg(cast(f.home_yellows as integer)+ cast(f.away_yellows as integer)),1) as avg_cards,\n" +
-            "f.league,\n" +
             "count(f.league) as matches\n" +
             "from rep_data.football f \n" +
             "where f.ref_name is not null \n" +
-            "group by ref_name,league";
+            "group by ref_name";
 
     @Override
     public void createReplicationFlow(SourceUrlEnum sourceUrlEnum) throws SQLException {
